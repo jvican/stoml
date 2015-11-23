16ed803e628187626c981b1d0ec9ff31d61ebfe7
jvican <jorgevc@fastmail.es>
2015-11-23 14:42:51 +0100

    core: parse comments correctly
    * Update to Fastparse 0.3.2
    
    * Add .idea to gitignore
    
    * Smart handling of whitespaces

--- build.sbt
+++ build.sbt
@@ -7,7 +7,7 @@ version := "1.0"
 scalaVersion := "2.11.7"
 
 libraryDependencies ++= Vector(
-  "com.lihaoyi" %% "scalaparse" % "0.2.1",
+  "com.lihaoyi" %% "fastparse" % "0.3.2",
   "org.scalacheck" %% "scalacheck" % "1.12.5" % "test",
   "org.scalatest" % "scalatest_2.11" % "2.2.4" % "test"
 )
--- src/main/scala/parser/TomlParser.scala
+++ src/main/scala/parser/TomlParser.scala
@@ -17,7 +17,6 @@ object Toml {
   case object True extends Bool {def v = true}
   case object False extends Bool {def v = false}
 
-  case class Comment(v: String) extends AnyVal with Elem
   case class Str(v: String) extends AnyVal with Elem
   case class Integ(v: Long) extends AnyVal with Elem
   case class Doub(v: Double) extends AnyVal with Elem
@@ -33,11 +32,12 @@ object Toml {
 }
 
 trait ParserUtil {
+  this: TomlSymbol =>
   import Toml.NamedFunction
-  val Whitespace = NamedFunction(" \t".contains(_: Char), "Whitespace")
+  val Whitespace = NamedFunction(WSChars.contains(_: Char), "Whitespace")
   val Digits = NamedFunction('0' to '9' contains (_: Char), "Digits")
   val Letters = NamedFunction((('a' to 'z') ++ ('A' to 'Z')).contains, "Letters")
-  val UntilNewline = NamedFunction((_: Char) != '\n', "UntilNewline")
+  val UntilNewline = NamedFunction(!NLChars._1.contains(_: Char), "UntilNewline")
 }
 
 trait TomlSymbol {
@@ -46,17 +46,22 @@ trait TomlSymbol {
   val Quotes = SingleQuote + DoubleQuote
   val Braces = ("[", "]")
   val Dashes = "-_"
+  val NLChars = ("\r\n", "\n")
+  val WSChars = " \t"
 }
 
 trait TomlParser extends ParserUtil with TomlSymbol {
   import fastparse.all._
   import Toml._
 
-  val space = P { CharsWhile(Whitespace).? }
-  val spaceL = P { space ~ CharIn("\n").? }
+  val newline = P(StringIn(NLChars._1, NLChars._2))
+  val charsChunk = P(CharsWhile(UntilNewline))
+  val comment: P0 = P { "#" ~ charsChunk.rep ~ &(newline | End) }
+  val WS0: P0 = P { CharsWhile(Whitespace) }
+  val WS: P0 = P { NoCut(NoTrace((WS0 | comment | newline).rep.?)) }
+
   val letters = P { CharsWhile(Letters) }
   val digits = P { CharsWhile(Digits) }
-  val comment = P { "#" ~ CharsWhile(UntilNewline, min=0) }.! map Comment
 
   val literalChars = NamedFunction(!SingleQuote.contains(_: Char), "LitStr")
   val basicChars = NamedFunction(!DoubleQuote.contains(_: Char), "BasicStr")
@@ -64,9 +69,9 @@ trait TomlParser extends ParserUtil with TomlSymbol {
   val escapedChars = P { CharsWhile(basicChars) | "\\\""}
 
   val basicStr: Parser[Str] =
-    P { DoubleQuote ~! escapedChars.rep.! ~ DoubleQuote } map Str
+    P { DoubleQuote ~/ escapedChars.rep.! ~ DoubleQuote } map Str
   val literalStr: Parser[Str] =
-    P { SingleQuote ~! unescapedChars.rep.! ~ SingleQuote } map Str
+    P { SingleQuote ~/ unescapedChars.rep.! ~ SingleQuote } map Str
   val string: Parser[Str] = P { basicStr | literalStr }
 
   def rmUnderscore(s: String) = s.replace("_", "")
@@ -89,26 +94,25 @@ trait TomlParser extends ParserUtil with TomlSymbol {
   val bareKey = P { (letters | digits | dashes).rep(min=1) }
   val validKey = P { bareKey | NoCut(basicStr) }.!
   lazy val pair: Parser[Pair] =
-    P { validKey ~ space ~ "=" ~ space ~ elem } map {
+    P { validKey ~ WS0.? ~ "=" ~ WS0.? ~ elem } map {
       kv: (String, Elem) => Pair(kv._1, kv._2)
     }
 
   lazy val array: Parser[Arr] =
-    P { "[" ~ spaceL ~ elem.rep(sep=space ~ "," ~! spaceL) ~ spaceL ~ "]" } map Arr
+    P { "[" ~ WS ~ elem.rep(sep=WS0.? ~ "," ~/ WS) ~ WS ~ "]" } map Arr
 
   val tableIds: Parser[Seq[String]] =
-    P { validKey.rep(min=1, sep=space ~ "." ~ space) }
+    P { validKey.rep(min=1, sep=WS0.? ~ "." ~ WS0.?) }
   val tableDef: Parser[Seq[String]] =
-    P { "[" ~ space ~ tableIds ~ space ~ "]" }
+    P { "[" ~ WS0.? ~ tableIds ~ WS0.? ~ "]" }
   val table: Parser[LMap] =
-    P { tableDef ~ spaceL ~ pair.rep(sep=spaceL) } map {
+    P { tableDef ~ WS ~ pair.rep(sep=WS) } map {
       t => LMap(t._1, t._2)
     }
 
-  lazy val elem: Parser[Elem] =
-    P {
-      space ~ (table | string | boolean | double | integer | array | comment) ~ space
-    }
+  lazy val elem: Parser[Elem] = P {
+    WS ~ (table | string | boolean | double | integer | array) ~ WS ~ End.?
+  }
 }
 
 object TomlParser extends TomlParser
