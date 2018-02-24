fd655a1114d69dbd20d9d3a82296ddd8d8f198cf
Tim Nieradzik <tim@sparse.tech>
2018-02-24 23:29:55 +0100

    TomlParser: Add full support for date and time formats
    Closes #3

--- src/test/scala/stoml/DateTomlSpec.scala
+++ src/test/scala/stoml/DateTomlSpec.scala
@@ -1,9 +1,10 @@
 package stoml
 
+import java.time._
+
 import org.scalacheck.Gen
 import org.scalatest.prop._
-import org.scalatest.{Matchers, PropSpec}
-import fastparse.core.Parsed.Success
+import org.scalatest.{FunSuite, Matchers, PropSpec}
 
 trait DateTomlGen {
   import Gen.chooseNum
@@ -15,19 +16,19 @@ trait DateTomlGen {
 
   /* This check is not covering all the formats */
   val dateFormatGen: Gen[String] = for {
-    day <- genNum(2, 0, 28)
+    day <- genNum(2, 1, 28)
     month <- genNum(2, 1, 12)
     year <- genNum(4, 0, 2200)
     hour <- genNum(2, 0, 23)
     minute <- genNum(2, 0, 59)
     second <- genNum(2, 0, 59)
     micro <- genNum(3, 0, 999)
-  } yield (year + "-" + month + "-" + day +
-           "T" + hour + ":" + minute + ":" + 
-           second + "." + micro + "Z")
+  } yield year + "-" + month + "-" + day +
+          "T" + hour + ":" + minute + ":" +
+          second + "." + micro + "Z"
 }
 
-class DateTomlSpec extends PropSpec 
+class DateTomlSpec extends PropSpec
     with PropertyChecks with Matchers 
     with DateTomlGen with TomlParser 
     with TestParserUtil {
@@ -35,7 +36,7 @@ class DateTomlSpec extends PropSpec
   property("parse dates following the RFC 3339 spec (`date` parser)") {
     forAll(dateFormatGen) {
       s =>
-        shouldBeSuccess(date.parse(s))
+        shouldBeSuccess(offsetDateTime.parse(s))
     }
   }
 
@@ -44,5 +45,69 @@ class DateTomlSpec extends PropSpec
       s =>
         shouldBeSuccess(elem.parse(s))
     }
+  }
+}
+
+class DataTomlUnitSpec extends FunSuite
+  with PropertyChecks with Matchers
+  with DateTomlGen with TomlParser
+  with TestParserUtil {
+
+  test("Parse local date") {
+    val toml = "ld = 1979-05-27"
+    val nodes = testSuccess(toml)
+    assert(nodes(0) == Toml.Pair("ld", Toml.Date(LocalDate.of(1979, 5, 27))))
+  }
+
+  test("Parse local time") {
+    val toml =
+      """
+        |lt1 = 07:32:00
+        |lt2 = 00:32:00.999999
+        |lt3 = 00:32:00.555
+      """.stripMargin
+
+    val nodes = testSuccess(toml)
+    assert(nodes(0) == Toml.Pair("lt1", Toml.Time(LocalTime.of(7, 32, 0, 0))))
+    assert(nodes(1) == Toml.Pair("lt2", Toml.Time(LocalTime.of(0, 32, 0, 999999000))))
+    assert(nodes(2) == Toml.Pair("lt3", Toml.Time(LocalTime.of(0, 32, 0, 555000000))))
+  }
+
+  test("Parse local date time") {
+    val toml =
+      """
+        |ldt1 = 1979-05-27T07:32:00
+        |ldt2 = 1979-05-27T00:32:00.999999
+      """.stripMargin
+    val nodes = testSuccess(toml)
+    assert(nodes(0) == Toml.Pair("ldt1", Toml.DateTime(LocalDateTime.of(
+      LocalDate.of(1979, 5, 27), LocalTime.of(7, 32, 0, 0)))))
+    assert(nodes(1) == Toml.Pair("ldt2", Toml.DateTime(LocalDateTime.of(
+      LocalDate.of(1979, 5, 27), LocalTime.of(0, 32, 0, 999999000)))))
+  }
+
+  test("Parse offset date time") {
+    val toml =
+      """
+        |odt1 = 1979-05-27T07:32:00Z
+        |odt2 = 1979-05-27T00:32:00-07:00
+        |odt3 = 1979-05-27T00:32:00.999999-07:00
+      """.stripMargin
+    val nodes = testSuccess(toml)
+    assert(nodes(0) == Toml.Pair("odt1", Toml.OffsetDateTime(
+      OffsetDateTime.of(
+        LocalDateTime.of(
+          LocalDate.of(1979, 5, 27), LocalTime.of(7, 32, 0)
+        ), ZoneOffset.of("Z")))))
+    assert(nodes(1) == Toml.Pair("odt2", Toml.OffsetDateTime(
+      OffsetDateTime.of(
+        LocalDateTime.of(
+          LocalDate.of(1979, 5, 27), LocalTime.of(0, 32, 0)
+        ), ZoneOffset.of("-07:00")))))
+    assert(nodes(2) == Toml.Pair("odt3", Toml.OffsetDateTime(
+      OffsetDateTime.of(
+        LocalDateTime.of(
+          LocalDate.of(1979, 5, 27), LocalTime.of(0, 32, 0, 999999000)
+        ), ZoneOffset.of("-07:00")))))
   }
 }
